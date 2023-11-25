5c8960222c8de1bf2bd1a135cefe7ce71aa46f48
wendyjazmin <fjchanataxi@uce.edu.ec>
2023-11-25 10:41:27 -0500

    taller9: estilos a la calculadora

--- js/calculadoraFuncionesDeber.js
+++ js/calculadoraFuncionesDeber.js
@@ -1,18 +1,18 @@
 function numero(numero){
-    document.getElementById("consola").value = document.getElementById("consola").value + numero;
+    document.getElementById("idIngreso").value = document.getElementById("idIngreso").value + numero;
 }
 
 function operacion(operacion){
-    document.getElementById("consola").value = document.getElementById("consola").value + operacion;
+    document.getElementById("idIngreso").value = document.getElementById("idIngreso").value + operacion;
 }
 
 function resultado(){
-    let resultado = document.getElementById("consola").value;
+    let resultado = document.getElementById("idIngreso").value;
     let total = eval(resultado);
-    document.getElementById("resultado").innerHTML="resultado: "+total;
+    document.getElementById("idResultado").innerHTML="resultado: "+total;
 }
 
 function reset(){
-    document.getElementById("consola").value="";
-    document.getElementById("resultado").innerHTML="resultado: ";
+    document.getElementById("idIngreso").value="";
+    document.getElementById("idResultado").innerHTML="resultado: ";
 }
